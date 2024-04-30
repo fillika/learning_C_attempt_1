a99206f2645d600cc769540fd23724fcbda33396
Aleksandr Mironenko <alfie.m1r@yandex.ru>
2024-04-30 10:47:28 +0300

    Finished first video

--- hello.c
+++ hello.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello World\n");
+int main()
+{
+    int goldPerGoblin = 15;
+    int goblinHp = 3;
+    char ch;
+
+    printf("You have been attacked by goblin.\n");
+    printf("Press (A)ttack or (R)un.\n");
+    ch = getchar();
+
+    while (ch != 'q' && ch != 'Q')
+    {
+        printf("You pressed %c.\n", ch);
+        if (ch == 65 || ch == 97)
+        {
+            goblinHp--;
+            printf("Attack goblin! Goblin HP: %d\n", goblinHp);
+        }
+        else if (ch == 'R' || ch == 'r')
+        {
+            printf("Run, Forest, Run!\n");
+            break;
+        }
+
+        if (goblinHp < 1)
+        {
+            printf("You killed goblin.\n");
+            printf("You picked up %d gold.\n", goldPerGoblin);
+            break;
+        }
+
+        printf("Loop ended\n");
+        getchar(); // clean buffer
+        ch = getchar();
+    }
+
+    printf("Game ended\n");
     return 0;
 }
